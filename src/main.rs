6e40b6a1b0d3e400787dbebbf14cdf5b76cc60aa
evilMorty <michele.xiloyannis@akina.health>
2023-05-26 08:59:20 +0200

    functions, closures, iterators

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,11 @@ fn main() {
 
 
     // CLOSURES ARE SIMILAR TO FUNCTIONS, BUT ARE NOT NAMED
+    // Closures differ from functions in that they can capture values from the scope they are defined in. 
+    // Isntead of defining a global variable, we can define a closure that captures the value from the scope it is defined in.
     let double = |d| d * 2; // |d| is the closure, d is the argument, d * 2 is the return value
- 
+    // this closure could capture a variable d in its scope, and return d * 2 outside of its scope
+    // functions cannot do this, they can only return values from within their scop
     // This is the outcome of calling the closure
     let var = double(10);
     println!("{}", var); // 20¨
@@ -173,10 +176,10 @@ fn main() {
     #[derive(Debug)]
     struct Chapter(String);
     
-    let number = 10;
+    let number = 2;
  
     if number <= 5 {
-        todo!("we will handle this outcome soon.")
+        println!("we will handle this outcome soon.")
     } 
     else if number > 5 {
         unimplemented!("we might do something here eventually")
@@ -185,7 +188,124 @@ fn main() {
         unreachable!()
     }
 
-    panic!("we should use panics sparingly.");
+    // CONDITIONALS
+    // if statements are similar to other languages.
+    // if statements can be used in let statements. if let statmenets allows us to descturcture a type and match it against a pattern.
+    let number = 5;
+
+    if number < 0 {
+        println!("The number is negative");
+    } else if number > 0 {
+        println!("The number is positive");
+    } else {
+        println!("The number is zero");
+    }
+
+    // Example of an if let statement
+    let some_result: Option<i32> = Some(42);
+
+    if let Some(value) = some_result {
+        println!("The value is {}", value);
+    } else {
+        println!("The result is None");
+    }
+
+
+    // FUNCTIONS 
+    // functions are defined using the fn keyword.
+    // functions can be generic.
+    // functions differ form closure in that they are not anonymous.
+    // functions differ from closures in that they do not capture their environment.
+    let out_of_scope = "sorry, this won't compile";
+
+    fn my_function() {
+      // This will not compile.
+      // println!("{out_of_scope}");
+     
+      let in_scope = "this will compile";
+      println!("{in_scope}");
+    }
+    // rust also supports arrow functions, similar to js
+    fn an_arrow_function() -> String {
+        "this is an arrow function".to_string()
+    }
+
+    println!("{}",an_arrow_function());
+
+    my_function();
+    // a closure could capture out_of_scope, but a function cannot.
+    let a_closure = || println!("{out_of_scope}");
+    a_closure();
+
+    // CLOSURES
+    // closures are anonymous functions.
+    // closures can capture their environment.
+    // closures can be stored in variables.
+    // closures are functions and can be passed as arguments to other functions.
+    // closures are defined using the || operator.
+    // closures can be defined using the move keyword, which will move the environment into the closure.
+    // closures can defined ina struct  
+    // unlike functions, closures are lazy. They will not be evaluated until they are called.
+    struct Magic { // a struct with a function field
+        field: fn(),
+    }
+    
+    let almost = Magic {
+        field: || println!("almost magic!"), // since closures are functions, we can use them as fields
+    };
+
+    (almost.field)(); // almost magic!
+
+    //Iterators
+    let numbers = [1,2,3];
+    let mut numbers = numbers.iter();
+
+    if let Some(first) = numbers.next() {
+        println!("{first}");
+    }
+    if let Some(second) = numbers.next() {
+        println!("{second}");
+    }
+
+    //collect
+    let mut other_numbers = [10, 20, 30].iter();
+    other_numbers.next();
+    other_numbers.next();
+     
+    let remaining_numbers: Vec<&u32> = other_numbers.collect(); // [30]
+    println!("{remaining_numbers:?}");
+
+    
+    //map
+    //The map method transforms each element of an iterator, using a closure. 
+    let numbers_map = [1, 2, 3];
+ 
+    let nums: Vec<i32> = numbers_map.iter()
+      .map(|x| x * 2 )
+      .collect();
+     
+    println!("{nums:?}"); // [2, 4, 6]     
+
+    //filter
+    //The filter method filters an iterator, using a closure.
+    // it returns values that satisfy a condition
+    let chars = ['a', '1', 'E', 'F'];
+ 
+    let filtered: Vec<&char> = chars.iter()
+    .filter(|c| c.is_uppercase())
+    .collect();
+    
+    println!("{filtered:?}"); // ['E', 'F']
+    
+    //enumerate
+    let names = ["Li", "Patrick", "Omar"];
+ 
+    let enumerated: Vec<(usize, &&str)> = names.iter()
+      .enumerate()
+      .filter( |(i, n)| i > &1)
+      .collect();
+     
+    println!("{enumerated:?}");  // [(2, "Omar")]    
 }
 
 // Attributes are used to annotate code and add metadata or functionality to it.
@@ -194,18 +314,3 @@ fn main() {
 pub fn is_true() {
     assert!(true, "successful test")
 }
-// attributes can also be outer attributes, applied to the scope they are in.
-
-#[derive(Debug, Clone)]
-struct Chair {
-    legs: u32,
-    wooden: bool,
-}
- 
-let chair = Chair {
-    legs: 4,
-    wooden: true,
-};
- 
-// Now we can print the debug output of our `Chair` type:
-println!("{chair:#?}")
